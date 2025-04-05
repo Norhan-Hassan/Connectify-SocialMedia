04352d63453598366c1f3d7956bfa19eab79a08f
Norhan Hassan <nwrhanhsnly@gmail.com>
2025-04-05 18:01:09 +0200

    initial

---
+++ SocialMedia/HUBs/ChatHub.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SocialMedia.HUBs
+{
+    public class ChatHub:Hub
+    {
+    }
+}
---
+++ SocialMedia/Repos/PostRepo.cs
@@ -0,0 +1,6 @@
+﻿namespace SocialMedia.Repos
+{
+    public class PostRepo:IPostRepo
+    {
+    }
+}
