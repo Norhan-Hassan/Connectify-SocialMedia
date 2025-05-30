b9119706c042738f16a9db2304f2606eeb9a99a2
Norhan Hassan <nwrhanhsnly@gmail.com>
2025-05-31 00:34:59 +0300

    some changes

--- SocialMedia/Repositories/IPokesRepo.cs
+++ SocialMedia/Repositories/IPokesRepo.cs
@@ -5,7 +5,7 @@ namespace SocialMedia.Repositories
     public interface IPokesRepo
     {
         Task<UserPoke> GetUserPokeAsync(string sourceUserId, string pokedUserId);
-        //Task<IEnumerable<PokeDto>> GetUserPokesAsync(string predicate, string userId);
+        Task<IEnumerable<PokeDto>> GetUserPokesAsync(string predicate, string userId);
         Task<ApplicationUser> GetUserWithLikes(string userId);
     }
 }
--- SocialMedia/Repositories/PokesRepo.cs
+++ SocialMedia/Repositories/PokesRepo.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SocialMedia.Data;
+using SocialMedia.DTOs;
 using SocialMedia.Models;
 
 namespace SocialMedia.Repositories
@@ -19,12 +20,28 @@ namespace SocialMedia.Repositories
             return userpoke;
         }
 
-        //public Task<IEnumerable<PokeDto>> GetUserPokesAsync(string predicate, string userId)
-        //{
-        //    var users = _context.ApplicationUsers.OrderBy(u => u.UserName).AsQueryable();
-        //    var pokes = _context.Pokes.AsQueryable();
+        public async Task<IEnumerable<PokeDto>> GetUserPokesAsync(string predicate, string userId)
+        {
+            var users = _context.ApplicationUsers.OrderBy(u => u.UserName).AsQueryable();
+            var pokes = _context.Pokes.AsQueryable();
+            if (predicate == "Poked")
+            {
+                pokes = pokes.Where(p => p.sourceUserId == userId);
+                users = pokes.Select(p => p.PokedUser);
+            }
 
-        //}
+            if (predicate == "PokedBy")
+            {
+                pokes = pokes.Where(p => p.pokedUserId == userId);
+                users = pokes.Select(p => p.SourceUser);
+            }
+            return await users.Select(user => new PokeDto
+            {
+                UserName = user.UserName,
+                PhotoUrl = user.Photos.FirstOrDefault(u => u.IsMainPhoto == true).PhotoUrl
+
+            }).ToListAsync();
+        }
 
         public async Task<ApplicationUser> GetUserWithLikes(string userId)
         {
